25f160cd93398a1db4aa0d09144a9e727065a403
Ajay <gangwar030@gmail.com>
2026-06-12 19:11:28 +0530

    Main Class file

---
+++ Backend/src/main/java/com/Assesment/CRUD/CrudApplication.java
@@ -0,0 +1,14 @@
+package com.Assesment.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+		System.out.println("CRUD Application has been started for operation of Todo App for Developer Role");
+	}
+
+}
