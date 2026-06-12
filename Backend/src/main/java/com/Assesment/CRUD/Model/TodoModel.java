58ecd97b7573de0eb946196a87af3c146dab381e
Ajay <gangwar030@gmail.com>
2026-06-12 19:05:24 +0530

    Todo Model

---
+++ Backend/src/main/java/com/Assesment/CRUD/Model/TodoModel.java
@@ -0,0 +1,24 @@
+package com.Assesment.CRUD.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "list")
+public class TodoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserModel user;
+}
