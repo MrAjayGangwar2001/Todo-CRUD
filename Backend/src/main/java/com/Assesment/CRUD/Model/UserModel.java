7fa05839f71e0468c2f3be840a14443ff88d5817
Ajay <gangwar030@gmail.com>
2026-06-12 19:05:49 +0530

    User Model

---
+++ Backend/src/main/java/com/Assesment/CRUD/Model/UserModel.java
@@ -0,0 +1,36 @@
+package com.Assesment.CRUD.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+
+    private String userName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER; // ← Default USER hoga
+
+    public enum Role {
+        USER, ADMIN
+    }
+
+
+}
